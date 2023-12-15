ce42981331e9cb0d66a8dc0eba5c355293fd588c
José Daniel <91283818+daniellsilva3@users.noreply.github.com>
2023-12-15 00:05:45 -0300

    Update linhasRoutes.js

--- Projeto-Backend/ExpLinhas/src/routes/linhasRoutes.js
+++ Projeto-Backend/ExpLinhas/src/routes/linhasRoutes.js
@@ -1,13 +1,12 @@
 import express from "express";
 import linhasController from '../controllers/linhasControllers.js'
 
-//metodo especifico  para gestão de rotas
+
 const routes = express.Router();
 
-//passar a rota e controller como parametro
-routes.get("/formulario", vagasController.listarVagas);
-routes.get("/formulario/:id", vagasController.listarVagasPorId);
-routes.post("/formulario", vagasController.cadastrarVaga);
-routes.put("/formulario/:id", vagasController.atualizarVaga);
+routes.get("/formulario", linhasController.listarLinhas);
+routes.get("/formulario/:id", linhasController.listarLinhasPorId);
+routes.post("/formulario", linhasController.atualizarlinhas);
+routes.put("/formulario/:id", linhasController.cadastrarLinha);
 
 export default routes;
