fd67a20b722927b99a28476f4981d71b1fb33d68
José Daniel <91283818+daniellsilva3@users.noreply.github.com>
2023-12-14 23:33:16 -0300

    Update linhasControllers.js

--- Projeto-Backend/ExpLinhas/src/controllers/linhasControllers.js
+++ Projeto-Backend/ExpLinhas/src/controllers/linhasControllers.js
@@ -2,82 +2,61 @@
 import linhas from "../models/linhas.js";
 
 
-/* criar nossa classe sem passar parametros sem retorno*/
+
 
 class linhasController{
 
-    /*
-        ****OBS FUNCAO  ASSICRONA *******
-        funções assicronas são aquels que acessam ou buscam algum tipo de recurso
-        em um dispositivo externo, como por exemplo um banco de dados.
-        Nesse tipo de função, precisamos que nosso código espere a resposta para 
-        executar a ação seguinte
-
-        ***OBS FUNCAO STATIC***
-        Quandor queresmo utilizar o metodo de uma funcao sem precisar criar 
-        uma instancia da classe, chamar eles de forma estatica. 
-
-        Criar um método de listarVagas que recebe dois parametros, o primeiro
-        é o controlador que vai manejar as requesições e o segundo  as respostas.
-    */
+ 
    static async listarLinhas(req, res){
     try{
         const listarLinhas = await linhas.find({});
         res.status(200).json(listarLinhas);
     }
     catch(erro){
-      res.status(500).json({message:`Erro ao listar vagas- ${erro.message}`})
+      res.status(500).json({message:`Erro ao listar Linhas- ${erro.message}`})
     }
    }
    //listar vagas por id
    static async listarLinhasPorId(req, res){
     try{
-        //id vai guardar a id passada na requisição
+      
         const id = req.params.id;
-        //findById método  mogoose para buscar por id
+        
         const linhaEncontrada = await linhas.findById({id});
         res.status(200).json(linhaEncontrada);
     }catch(erro){
-        res.status(500).json({message:`erro ao listar vaga - ${erro.message}`});
+        res.status(500).json({message:`erro ao listar Linha - ${erro.message}`});
     }
    }
-//atualizar vagas por id
+
    static async atualizarlinhas(req, res){
     try{
-      /* id vai guardar a id passada na requisição
-      findByIdAndUpdate método do moogose que recebe dois parâmetros
-      a id e osdados da vaga atualizada. 
-      */
+   
       const id = req.params.id
       await vaga.findByIdAndUpdate(id, req.body)
-      res.status(200).json({message:`Vaga atualizada com sucesso!`})
+      res.status(200).json({message:`Linha atualizada com sucesso!`})
 
     }catch(erro){
-      res.status(500).json({message:`erro ao atualizar vaga ${erro.message}`})
+      res.status(500).json({message:`erro ao atualizar Linha ${erro.message}`})
     }
    }
 
 
-   //criar controller para cadastro de vagas, utilizando bloco try e catch
+ 
    static async cadastrarLinha(req, res){
 
-   //manejo de erro | SUCESSO
+
    try{
-        /*
-        Vaga é o modelo criado na camada models no mogoose e create é um 
-        metodo do mogoose para criar uma vaga no banco, e objeto de vaga
-        é passado no req.body
-        */
-       const vagaNova = await vaga.create(req.body);
-       //vagaNova é  retorno  do metodo create. 
-       res.status(201).send({message:'Vaga cadastrada com sucesso!', vaga:vagaNova});
+        
+       const vagaNova = await linhas.create(req.body);
+      
+       res.status(201).send({message:'Linha cadastrada com sucesso!', linhas:linhaNova});
    }
    catch(erro){
-     //500 erro interno do servidor
-     //erro.message é a mensagem de erro que o mongoose retorna
-     res.status(500).json({error:`${erro.message}-falha ao cadastrar vaga`});
+     
+     res.status(500).json({error:`${erro.message}-falha ao cadastrar Linha`});
    }
  }
 }
-//exportar classe para ser utilizada em outras partes da aplicação
+
 export default linhasController;
