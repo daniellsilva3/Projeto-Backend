c37764c09b8d6a164b2f6cd1d6ea76c37d103d63
José Daniel <91283818+daniellsilva3@users.noreply.github.com>
2023-12-15 01:10:47 -0300

    Update dbConnect.js

--- Projeto-Backend/ExpLinhas/src/config/dbConnect.js
+++ Projeto-Backend/ExpLinhas/src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function ConectaNaDatabase(){
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://jd64320:admin123@cluster0.v1da4by.mongodb.net/ExpressoLinhas?retryWrites=true&w=majority");
 
 
 return mongoose.connection;
